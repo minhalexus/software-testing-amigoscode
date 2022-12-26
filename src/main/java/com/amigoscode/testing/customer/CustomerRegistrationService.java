6c69d9dcfa223004ea29eb64f7d3c35756ad293e
Minhal Shanjer <mshanjer@gmail.com>
2022-12-25 21:22:15 -0500

    Made customer and unit tests.

---
+++ src/main/java/com/amigoscode/testing/customer/CustomerRegistrationService.java
@@ -0,0 +1,44 @@
+package com.amigoscode.testing.customer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class CustomerRegistrationService {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerRegistrationService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public void registerNewCustomer(CustomerRegistrationRequest request) {
+        // 1. PhoneNumber is taken
+        // 2. if taken check if belongs to same customer
+        // 3. if yes then return else throw exception
+        // 4. Save Customer
+
+
+        String phoneNumber = request.getCustomer().getPhoneNumber();
+        Optional<Customer> customerOptional = customerRepository.selectCustomerByPhoneNumber(phoneNumber);
+
+        if (customerOptional.isPresent()) {
+            Customer customer = customerOptional.get();
+            if (customer.getName().equals(request.getCustomer().getName())) {
+                return;
+            }
+            throw new IllegalStateException(String.format("phone number [%s] is taken", phoneNumber));
+        }
+
+        if(request.getCustomer().getId() == null) {
+            request.getCustomer().setId(UUID.randomUUID());
+        }
+
+        customerRepository.save(request.getCustomer());
+    }
+
+}
